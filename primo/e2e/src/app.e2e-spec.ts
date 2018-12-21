7dad07b4d54f23fa08fc1aaf9bc7525fa52d7ddd
João Bosco de Barros Freitas <jbbfreitas@gmail.com>
2018-12-21 13:28:54 -0300

    Alterado o teste unitário

--- primo/e2e/src/app.e2e-spec.ts
+++ primo/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to primo!');
+    expect(page.getTitleText()).toEqual('Olá Mundo! Seja bem-vindo ao');
   });
 });
